a854f088965af99c640444432cf6378959daf7c8
Hake Pratiksha <86944858+pratikshahake26@users.noreply.github.com>
2022-05-20 12:07:44 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,9 @@
+public user class{
+      public  String name ;
+       public String email;
+       public String phone  ;
+       public String Date  ;
+       public String Time  ;
+       public String Department;
+      public  String Symptoms ;
+}
